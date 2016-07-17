c4371e336ecccacbc361e229bb672afe0fe32bb9
Amy Chan <chamy09@gmail.com>
2016-07-17 15:34:34 -0500

    fixed paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_region: 'us-west-2',
-    s3_host_name: ENV['AWS_HOSTNAME'],
+    url: :s3_domain_url,
     s3_credentials: {
       bucket: ENV['AWS_S3_BUCKET'],
       access_key_id: ENV['AWS_ACCESS_KEY_ID'],
